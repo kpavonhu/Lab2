17aac1daa86c5b846d1dc11e554e03a5906e6374
kpavonhu <112138880+kpavonhu@users.noreply.github.com>
2024-05-07 13:12:51 -0600

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,12 +1,12 @@
 output "cluster_name" {
-  value = google_container_cluster.default.name
+  value = google_container_cluster.lab-2.name
 }
 output "cluster_endpoint" {
-  value = google_container_cluster.default.endpoint
+  value = google_container_cluster.lab-2.endpoint
 }
 output "cluster_location" {
-  value = google_container_cluster.default.location
+  value = google_container_cluster.lab-2.location
 }
 output "load-balancer-ip" {
-  value = google_compute_address.default.address
+  value = google_compute_address.lab-2.address
 }
