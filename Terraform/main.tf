b3893475e6360b5709828e39113556aadf2474d5
kpavonhu <112138880+kpavonhu@users.noreply.github.com>
2024-05-07 22:28:51 -0600

    main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "lab-2" {
   name               = "my-first-cluster"
   location           = "us-central1-c"
   initial_node_count = 3
-  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  min_master_version = data.google_container_engine_versions.lab-2.latest_master_version
 
   node_config {
     machine_type = "g1-small"
